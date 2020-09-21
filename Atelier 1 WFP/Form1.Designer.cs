badd1e107cd2a53d66173079843eb34f00083240
Romgb <romain.gilibert5@gmail.com>
2020-09-21 17:32:33 +0200

    Ajoutez des fichiers projet.

---
+++ Atelier 1 WFP/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System.Windows.Forms;
+
+namespace Atelier_1_WFP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.bntEffacer = new System.Windows.Forms.Button();
+            this.bntRecopier = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.btnQuitter);
+            this.groupBox1.Controls.Add(this.bntEffacer);
+            this.groupBox1.Controls.Add(this.bntRecopier);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(748, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(646, 106);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 3;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitterApp_Click);
+            // 
+            // bntEffacer
+            // 
+            this.bntEffacer.Location = new System.Drawing.Point(646, 69);
+            this.bntEffacer.Name = "bntEffacer";
+            this.bntEffacer.Size = new System.Drawing.Size(75, 23);
+            this.bntEffacer.TabIndex = 2;
+            this.bntEffacer.Text = "Effacer";
+            this.bntEffacer.UseVisualStyleBackColor = true;
+            this.bntEffacer.Click += new System.EventHandler(this.btnEffacerSel1_Click);
+            // 
+            // bntRecopier
+            // 
+            this.bntRecopier.Location = new System.Drawing.Point(646, 33);
+            this.bntRecopier.Name = "bntRecopier";
+            this.bntRecopier.Size = new System.Drawing.Size(75, 23);
+            this.bntRecopier.TabIndex = 1;
+            this.bntRecopier.Text = "Recopier";
+            this.bntRecopier.UseVisualStyleBackColor = true;
+            this.bntRecopier.Click += new System.EventHandler(this.btnCopieSel1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Copie :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(72, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(323, 13);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 198);
+            this.Controls.Add(this.groupBox1);
+            this.Location = new System.Drawing.Point(45, 0);
+            this.Name = "Form1";
+            this.Text = "Recopier un champ dans un autre";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button bntEffacer;
+        private System.Windows.Forms.Button bntRecopier;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
+
---
+++ Atelier 1 WFP/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atelier_1_WFP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       #region Contrôles Formulaire
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopieSel1_Click(object sender, EventArgs e)
+        {
+            textBox2.Multiline = false;
+            textBox2.Height = 20;
+            textBox2.Clear();
+
+            textBox1.SelectionStart = 0;
+            textBox1.SelectionLength = textBox1.TextLength;
+            textBox2.Text = textBox1.SelectedText;
+            
+        }      
+
+        private void btnEffacerSel1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void btnQuitterApp_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+
+
+    
+
+
+    }
+}
